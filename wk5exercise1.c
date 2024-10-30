a5777a976da02a69d325d97a4daf651045c42331
Trudy Wong <trudypwong@gmail.com>
2024-10-30 15:48:33 -0700

    Fixing CWE-78 in exercise1

--- wk5exercise1.c
+++ wk5exercise1.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> // Required for execvp
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   char *userName = argv[1];
-  
-    char command1[1000] = {0};
-    sprintf(command1, "ls %s", userName);
-    system(command1);
+
+  char command1[1000] = {0};
+
+  // Original
+  // sprintf(command1, "ls %s", userName);
+
+  // Fix 1 - safer because explicitly specifying the full path
+  execl("/bin/ls", "ls", userName, NULL);
+
+  // Fix 2 - more flexible as it searches the PATH
+  // char *args[] = {"ls", "-l", userName};
+  // execvp("ls", args);
+
+  system(command1);
 }
