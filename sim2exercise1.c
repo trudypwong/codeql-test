b8b5864b6d644fc2f3bf8301bd10953828c8604d
Trudy Wong <trudypwong@gmail.com>
2024-11-03 21:41:21 -0800

    Fix for Simulation2 exercise

--- sim2exercise1.c
+++ sim2exercise1.c
@@ -13,7 +13,13 @@ int main(int argc, char **argv)
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
+    // original
+    // char cmd[BUFSIZE] = "wc -c < ";
+    // strcat(cmd, argv[1]);
+
+    // fix
+    char cmd[BUFSIZE];
+    snprintf(cmd, BUFSIZE, "wc -c < %.20s", argv[1]);
+
     system(cmd);
 }
