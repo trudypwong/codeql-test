6d4d0875401126eb0e071c972f2cebd5dfe4f999
Trudy Wong <trudypwong@gmail.com>
2024-11-03 22:27:08 -0800

    Fix #2 for simulation2 exercise

--- sim2exercise1.c
+++ sim2exercise1.c
@@ -1,8 +1,25 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define BUFSIZE 256
+
+// Function to validate input
+int is_valid_input(const char *input)
+{
+    size_t length = strlen(input); // Get the length of the string
+    // Iterate over each character in the string using the length
+    for (size_t i = 0; i < length; i++)
+    {
+        if (input[i] == '/' || input[i] == ';' || input[i] == '&' || input[i] == '<' || input[i] == '>')
+        {
+            printf("Character %zu: %c\n", i, input[i]);
+            return 0; // Invalid character found
+        }
+    }
+    return 1; // Input is valid
+}
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char **argv)
@@ -17,7 +34,14 @@ int main(int argc, char **argv)
     // char cmd[BUFSIZE] = "wc -c < ";
     // strcat(cmd, argv[1]);
 
-    // fix
+    // Validate user input
+    if (!is_valid_input(argv[1]))
+    {
+        fprintf(stderr, "Invalid input: '/', ';', '&', '<', '>' are not allowed, only alphanumeric letters allowed.\n");
+        return 1;
+    }
+
+    // Use snprintf instead of strcat
     char cmd[BUFSIZE];
     snprintf(cmd, BUFSIZE, "wc -c < %.20s", argv[1]);
 
